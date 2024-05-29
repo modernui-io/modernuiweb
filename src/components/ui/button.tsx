d99971227cbc0798e992a373a072b72431805ceb
Nnenna John <nnennajohn@gmail.com>
2024-05-28 22:42:22 -0400

    Update primary color (#12)
    * Update primary color
    
    * Add theme toggler to storybook

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -7,12 +7,13 @@ import { Loader2Icon } from "lucide-react";
 import { cn } from "~/lib/utils";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-1 whitespace-nowrap rounded-full text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50",
+  "inline-flex items-center justify-center gap-1 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50",
   {
     variants: {
       variant: {
         default:
           "bg-primary text-primary-foreground shadow hover:bg-primary/90",
+        brand: "bg-brand text-brand-foreground shadow hover:bg-brand/90",
         destructive:
           "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
         outline:
@@ -24,7 +25,7 @@ const buttonVariants = cva(
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
         getApp:
-          "border-0 bg-primary-700 text-background hover:bg-primary-600 focus:outline-none focus:ring-4 focus:ring-gray-300",
+          "border-0 bg-brand-700 text-background hover:bg-brand-600 focus:outline-none focus:ring-4 focus:ring-gray-300",
       },
       size: {
         default: "h-9 px-5 py-2",
